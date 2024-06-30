bb5eac19b0d234a331750437336f10c4c8575d4e
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-06-30 18:52:40 +0530

    ..

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const senderName = document.querySelector("#sender");
 receiverName.innerText = new URLSearchParams(location.search).get("receiver");
 senderName.innerText =
   new URLSearchParams(location.search).get("sender") || "Kaustubhya Shukla";
+
+//   http://127.0.0.1:5500/?receiver=mom%20and%20dad&sender=saksham
